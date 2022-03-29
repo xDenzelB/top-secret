a064f2f44447925c1aae7e8a3d71d58591fe05ad
Denzel <dbartolaba123@gmail.com>
2022-03-29 12:48:49 -0700

    added in my routes for app.js

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,10 +1,19 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS confidential 
+DROP TABLE IF EXISTS confidential CASCADE; 
 
 CREATE TABLE confidential (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     title TEXT NOT NULL,
     description TEXT NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP TABLE IF EXISTS user CASCADE;
+
+CREATE TABLE user (
+    user_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    email TEXT NOT NULL,
+    password_hash TEXT NOT NULL
+
 );

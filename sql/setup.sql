2aa99e6439f4b53ab7e5d093b72dcc8ce1af3577
Denzel <dbartolaba123@gmail.com>
2022-03-29 16:39:17 -0700

    got my first test to pass!

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,7 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS confidential CASCADE; 
+DROP TABLE IF EXISTS users CASCADE;
 
 CREATE TABLE confidential (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -9,11 +10,8 @@ CREATE TABLE confidential (
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
-DROP TABLE IF EXISTS user CASCADE;
-
-CREATE TABLE user (
+CREATE TABLE users (
     user_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     email TEXT NOT NULL,
     password_hash TEXT NOT NULL
-
 );

ec9aabff9b686273ce9542406054f33ca0ca6571
Denzel <dbartolaba123@gmail.com>
2022-03-29 14:15:28 -0700

    created my first test that signs up a user

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -11,4 +11,12 @@ describe('top-secret routes', () => {
   afterAll(() => {
     pool.end();
   });
+
+  it('signs up a new user', async () => {
+    const res = await request(app)
+    .post('/api/v1/users/signup')
+    .send({ email: 'ilovechicken@gmail.com', password: 'chickenislife'});
+
+    expect(res.body).toEqual({ user_id: expect.any(String), email: 'ilovechicken@gmail.com'})
+  })
 });

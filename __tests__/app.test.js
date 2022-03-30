633625c6081d31a031b27b8a458778f0d9dc28c7
Denzel <dbartolaba123@gmail.com>
2022-03-30 14:36:24 -0700

    Got my test to pass for logging out

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,8 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 const UserService = require('../lib/services/UserService');
+const { send } = require('express/lib/response');
+const { agent } = require('supertest');
 
 describe('top-secret routes', () => {
   beforeEach(() => {
@@ -26,14 +28,13 @@ describe('top-secret routes', () => {
       email: 'denzel@defence.gov',
       password: 'balut',
     });
-
-    const res = await request(app)
+    const agent = request.agent(app);
+    const res = await agent
     .post('/api/v1/users/signin')
     .send({
       email: 'denzel@defence.gov',
       password: 'balut'
     });
-
     expect(res.body).toEqual({
       message: 'You have successfully signed in!',
       user
@@ -41,19 +42,25 @@ describe('top-secret routes', () => {
   });
 
   it('should be able to logout a user', async () => {
-    let user = await UserService.create({
+    const agent = request.agent(app);
+    await UserService.create({
       email: 'denzel@defence.gov',
       password: 'balut',
     });
 
-    user = await UserService.signIn({
+  const user = await agent
+    .post('/api/v1/users/signin')
+    .send({
       email: 'denzel@defence.gov',
       password: 'balut',
     });
 
-    const res = request(app)
+    const res = await agent
     .delete('/api/v1/users/sessions')
-    .send(user);
+    .send({ 
+      email: user.email,
+      password: 'balut',
+     });
 
     expect(res.body).toEqual({
       message: 'Successfully signed out',
